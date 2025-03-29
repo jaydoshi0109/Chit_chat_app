a7106337675f4dfd17bd06cec08f90988ba6e97f
jaydoshi0109 <jaydoshi0109@gmail.com>
2025-03-29 12:58:26 +0530

    Fix types and structure for Render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,24 @@ import messageRoutes from "./routes/message.route";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket";
-
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// ✅ Setup allowed origin
+const clientOrigin =
+  process.env.NODE_ENV === "production"
+    ? process.env.FRONTEND_URL
+    : "http://localhost:5173";
+
+// ✅ Add this middleware to Express
+app.use(
+  cors({
+    origin: clientOrigin,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
